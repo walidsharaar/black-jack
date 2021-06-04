5b4704160d639b49a5a375e774d56742032d11ff
Sharaar <walidsharar@gmail.com>
2021-06-04 18:26:52 +0200

    Deal cards logic implemented

--- main.py
+++ main.py
@@ -15,10 +15,37 @@
 
 #todo 3 : create list of the card numbers
 
+import random
+# cards =[Ace,2,3,4,5,6,7,8,9,10,Jack,Queen,King]
+cards_number = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+
 
 #todo 4: create function for 2 selected card of user and computer
+"""returns random cards"""
+def deal_card():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card=random.choice(cards)
+    return card
+# empty lists to append the random generate item to it
+user_card=[]
+computer_card=[]
 
+#we used underscore cause we don't the variable of for loop
+for _ in range(2):
+    user_card.append(deal_card())
+    computer_card.append(deal_card())
 
 #todo 5: Create function that take a list of cards as input and return the result
+
+def score_sum_up(cards):
+# Inside the method check for a blackjack and return 0 instead of the actual score cause 0 will represent a black in the game
+    if 11 in cards and 10 in cards and len(cards)==2:
+        return 0
+#Checking ace (11) if the score is over 21, will remove 11 and replace it one
+    if 11 in cards and sum(cards)>21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
 #todo 6: sum the result and annouce the winner
