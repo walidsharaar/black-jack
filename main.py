d656e47452185b84c7201c6ccdc834284be606c1
Sharaar <walidsharar@gmail.com>
2021-06-04 18:07:33 +0200

    Project breakdown

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+############### The Game Rules #####################
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+## The deck is unlimited in size.
+## There are no jokers.
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+#todo 1: Go to this website and try out the Blackjack game: https://games.washingtonpost.com/games/blackjack/
+
+#todo 2: Create flowchart
+
+#todo 3 : create list of the card numbers
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#todo 4: create function for 2 selected card of user and computer
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#todo 5: Create function that take a list of cards as input and return the result
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#todo 6: sum the result and annouce the winner
